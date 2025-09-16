2050a35a96aad7abcb60b64c81eda1ddc77433f9
Surya1-2-3 <140395855+Surya1-2-3@users.noreply.github.com>
2025-09-16 21:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+const element=document.getElementById('btn')
+const input=document.getElementById("text-field")
+
+function submit(){
+	const data=input.value
+	if(data.length==0){
+		alert("todo item is empty")
+	}else{
+		const ele=document.createElement('li');
+		ele.classList.add("todo-item")
+		ele.innerHTML=data;
+		const parent=document.getElementById("todolist")
+		parent.appendChild(ele);
+		input.value=""
+	}
+}
